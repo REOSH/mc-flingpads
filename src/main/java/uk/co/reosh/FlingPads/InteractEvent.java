310b7a1470f7084ccd2ddaa038314a5d01d70098
CrossPL <norbud5@gmail.com>
2014-03-28 09:25:12 +0100

    Modified .gitignore, added license to source files, changed versioning, fixed plugin.yml, minor code changes.

--- src/main/java/uk/co/reosh/FlingPads/InteractEvent.java
+++ src/main/java/uk/co/reosh/FlingPads/InteractEvent.java
@@ -1,8 +1,24 @@
+/* MC-FlingPads
+ * Copyright (C) 2013 - 2014 REOSH Group and contributors
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package uk.co.reosh.FlingPads;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,21 +26,20 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class InteractEvent implements Listener {
+	private int boostStrength;
 
-	public static int boostStrength = FlingPads.getConfig().getInt("boost-strength");
+    public InteractEvent(int boostStrength) {
+        this.boostStrength = boostStrength;
+    }
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onInteract(PlayerInteractEvent event) {
-		final Block b = event.getClickedBlock();
-
-		if (event.getAction().equals(Action.PHYSICAL)) {
-			if (isPressurePlate(b.getType())) {
-				Player p = event.getPlayer();
-				p.setVelocity(p.getVelocity().add(p.getEyeLocation().getDirection().normalize().multiply(boostStrength).setY(0)));
-			}
+		if (event.getAction().equals(Action.PHYSICAL) && this.isPressurePlate(event.getClickedBlock().getType())) {
+			event.getPlayer().setVelocity(event.getPlayer().getVelocity().add(event.getPlayer().getEyeLocation().getDirection().normalize().multiply(this.boostStrength).setY(0)));
 		}
 	}
-	public static boolean isPressurePlate(Material mat) {
-		return (mat == Material.STONE_PLATE) || (mat == Material.WOOD_PLATE);
+
+	private boolean isPressurePlate(Material mat) {
+		return (mat == Material.STONE_PLATE || mat == Material.WOOD_PLATE);
 	}
 }
